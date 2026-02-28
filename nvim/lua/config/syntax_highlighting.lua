27c3449fc5d42feff9f848b81a320d8c9bb2dbb9
KishonShrill <crystalbluew@gmail.com>
2026-02-28 20:07:59 +0800

    fix: refactor copyparty config name; chore: syntax highlight ini

--- nvim/lua/config/syntax_highlighting.lua
+++ nvim/lua/config/syntax_highlighting.lua
@@ -5,5 +5,11 @@ vim.filetype.add({
     },
     pattern = {
         ['%.qss$'] = 'css', -- Treat files ending with .qss as CSS
+        ['%.config'] = 'ini',
+        ['%.conf'] = 'ini',
     },
 })
+
+-- ini files
+vim.api.nvim_set_hl(0, "@markup.heading.ini", { fg = "#C4A4DB", bold = true })
+vim.api.nvim_set_hl(0, "@comment.ini", { fg = "#74B386" })
